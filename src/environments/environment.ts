91648af0b18b3a5ad9fc83b0798e2e8705a35fa7
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2024-04-23 18:38:00 -0400

    read environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  baseUrl: 'https://nest-diagramador-production.up.railway.app/api',
+  url: 'https://nest-diagramador-production.up.railway.app',
+
+  // baseUrl: 'http://localhost:3000/api',
+  // url: 'http://localhost:3000',
+};
